2f66f18918d73eeaa40f2e0561068bae427e4df0
Noah Axon <n0xa@h-i-r.net>
2023-10-13 23:36:56 -0500

    Minimalize it for StickC-Plus

--- WiFiChanViz.ino
+++ WiFiChanViz.ino
@@ -1,12 +1,6 @@
 #include <WiFi.h>
 #include <ESP32-Chimera-Core.h> // https://github.com/tobozo/ESP32-Chimera-Core
 
-#define SDU_APP_NAME   "WiFi ChanViz"         // app title for the sd-updater lobby screen
-#define SDU_APP_PATH   "/WiFiChanViz.bin"     // app binary file name on the SD Card (also displayed on the sd-updater lobby screen)
-#define SDU_APP_AUTHOR "@tobozo"              // app binary author name for the sd-updater lobby screen
-#define SDU_APP_URL    "https://git.io/JMxaN" // app project URL also for lobby screen
-#include <M5StackUpdater.h>  // https://github.com/tobozo/M5Stack-SD-Updater
-
 // just some over-engineered demoscene fonts for LGFX, ripped and converted to
 // sprites from https://opengameart.org/content/new-original-grafx2-font-collection
 #include "ImgFont.h"
@@ -27,18 +21,16 @@ uint32_t scanChanDelay = 150; // default
 //#define USE_SCREENSHOTS
 #define channels_count 13
 
-#define SPRITE_WIDTH   240 // calibrated to work without psram
-#define SPRITE_HEIGHT  96  // calibrated to work without psram
+#define SPRITE_WIDTH   120 // calibrated to work without psram
+#define SPRITE_HEIGHT  180  // calibrated to work without psram
 
-#define GRAPH_HEIGHT   64 // smaller than sprite height, leave some pixels for perspective
+#define GRAPH_HEIGHT   150 // smaller than sprite height, leave some pixels for perspective
 #define GRAPH_PXOFF_Y   2
 
 const float invX          = 1.0-(1.5/float(GRAPH_HEIGHT));
 const float invY          = 1.0-(1.5/float(SPRITE_WIDTH));
 const float barWidth      = SPRITE_WIDTH / (channels_count);
 const uint32_t bottomY    = SPRITE_HEIGHT-1; // sprite bottom
-const String apptitle     = SDU_APP_NAME;
-const String copyrightURL = SDU_APP_URL;
 size_t avgcount           = 0;
 
 int32_t spritePosX, spritePosY; // where the graph will be rendered
@@ -51,13 +43,11 @@ uint32_t delayPosX,
          devCountPosY,
          devCountWidth = 110,
          devCountHeight = 24,
-         marginX = 40,
-         marginY = 10
+         marginX = 0,
+         marginY = 210
 ;
 
 std::array<int,channels_count> totaldpc;
-
-
 
 void drawUI()
 {
@@ -68,38 +58,12 @@ void drawUI()
   }
   tft.drawRect( spritePosX-1, spritePosY-1, SPRITE_WIDTH+2, SPRITE_HEIGHT+2, (RGBColor){0xcc, 0xcc, 0xcc} );
 
-  spriteMask->setFont( &Font8x8C64 );
-  spriteMask->setTextDatum( MC_DATUM );
-  spriteMask->setTextSize( 1.75, 2.25 );
-  spriteMask->drawString( "Scanning Started", spriteMask->width()/2, spriteMask->height()/2 );
-  spriteMask->pushSprite( spritePosX, spritePosY );
-
-  tft.fillRect( 18, 10, 56, 56, TFT_WHITE );
-  tft.drawPng( qr_code_png, qr_code_png_len, 21, 13 ); // https://git.io/JMxaN
-
   charDrawer_t charDrawer;
-  charDrawer.drawString( fontSprite, SDU_APP_NAME, 92, 12, &saikyoBlack, -1,       -1, 2.0 );
   // init sprite in native (8bit) color mode
   charDrawer.drawString( fontSprite, "?", -100, -100, &moor4bit, 0x000000U,  -1, 1.0 );
-  moor4bit.bit_depth = 1; // draw shadow first in 1bit mode
-  charDrawer.drawString( fontSprite, "By " SDU_APP_AUTHOR, 94, 39, &moor4bit, 0x000000U,  -1, 1.0 );
-  moor4bit.bit_depth = 8; // now draw regular
-  charDrawer.drawString( fontSprite, "By " SDU_APP_AUTHOR, 92, 37, &moor4bit, 0xffffffU,  -1, 1.0 );
 
-  // project URL
-  tft.setTextDatum( TL_DATUM );
-  tft.setTextColor( TFT_BLACK );
-  tft.setTextSize( 1, 1 );
-  tft.drawString( copyrightURL, 93, 58 );
-  tft.setTextColor( TFT_WHITE );
-  tft.drawString( copyrightURL, 92, 57 );
-
-  delayPosX    = marginX;
-  delayPosY    = tft.height()-(delayHeight+marginY);
   devCountPosX = tft.width()-(devCountWidth+marginX);
-  devCountPosY = delayPosY;
-
-  drawCaption( String("Ms/Ch: "+String(scanChanDelay)).c_str(), delayPosX, delayPosY, delayWidth, delayHeight );
+  devCountPosY = tft.height()-(delayHeight+marginY);
 
   tft.setFont( &Font8x8C64 );
   tft.setTextDatum( TL_DATUM );
@@ -174,8 +138,6 @@ void setup()
   spriteMask->setColorDepth(16);
   fontSprite->setColorDepth(1);
 
-  checkSDUpdater( SD, MENU_BIN, 5000, TFCARD_CS_PIN );
-
   #if defined USE_SCREENSHOTS
     M5.ScreenShot->begin();
   #endif
@@ -239,16 +201,14 @@ void loop()
       if( doscreenshot ) M5.ScreenShot->snap();
     #endif
 
-    if( M5.BtnC.wasPressed() ) {
+    if( M5.BtnB.wasPressed() ) {
       if( scanChanDelay < 500 ) {
         scanChanDelay += 50;
-        drawCaption( String("Ms/Ch: "+String(scanChanDelay)).c_str(), delayPosX, delayPosY, delayWidth, delayHeight );
       }
     }
-    if( M5.BtnB.wasPressed() ) {
+    if( M5.BtnA.wasPressed() ) {
       if( scanChanDelay > 50 ) {
         scanChanDelay -= 50;
-        drawCaption( String("Ms/Ch: "+String(scanChanDelay)).c_str(), delayPosX, delayPosY, delayWidth, delayHeight );
       }
     }
   } else if (n > 0) {
@@ -275,9 +235,4 @@ void loop()
     drawCaption( String("Count: "+String(n)).c_str(), devCountPosX, devCountPosY, devCountWidth, devCountHeight );
     WiFi.scanDelete();
   }
-
-
 }
-
-
-
